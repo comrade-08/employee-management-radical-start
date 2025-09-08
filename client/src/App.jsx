0246379a01bd7bd250949adf8c8ee35538c80223
Oldcycle123 <jeevakrishnan3219@gmail.com>
2025-09-08 09:36:01 +0530

    fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import CalendarPage from "./pages/CalendarPage";
 import NotFound from "./pages/NotFound";
 
 function App() {
+  console.log(import.meta.env.VITE_API_URL || "" , 'VITE_API_URL')
   return (
     <BrowserRouter>
       <Routes>
